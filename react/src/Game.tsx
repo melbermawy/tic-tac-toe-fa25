650857dcc8c56711a05a64de3d9fa64aca69d6ba
Mohamad Abdou <mohamedabdouelbermawy@gmail.com>
2025-09-24 18:14:07 -0400

    Added a helper function to create a new initial state and connected the get/:id and post/:id

--- react/src/Game.tsx
+++ react/src/Game.tsx
@@ -1,62 +1,82 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import type { GameState, Player } from "./tictactoe";
-import { initialGameState } from './tictactoe';
+import { useState, useEffect } from "react";
+import "./App.css";
+import type { GameState } from "./tictactoe";
+import { initialGameState } from "./tictactoe";
 
 function Game() {
-  const [gameState, setGameState] = useState<GameState>(initialGameState)
-
+  const [gameState, setGameState] = useState<GameState>(initialGameState);
+  const gameId = 1
 
   useEffect(() => {
-  fetch("/api/game")
-  .then(res => res.json())
-  .then((data: GameState) => setGameState(data))
-  .catch(err => console.error("error", err))
-   }, [])
+    fetch(`/api/game/${gameId}`)
+      .then((res) => res.json())
+      .then((data: GameState) => setGameState(data))
+      .catch((err) => console.error("error", err));
+  }, []);
 
-
-    async function handleClick(row: number, col: number) {
-      if (gameState.winner || gameState.board[row][col]) return
-      const updated = await fetch("/api/move", {
+  async function handleClick(row: number, col: number) {
+    if (gameState.winner || gameState.board[row][col]) return;
+    const updated = await fetch(`/api/move/${gameId}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ row, col })
-      })
-      .then(r => r.json())
-      setGameState(updated);
-    }
+      body: JSON.stringify({ row, col }),
+    }).then((r) => r.json());
+    setGameState(updated);
+  }
 
-    const parentDiv = "min-h-screen w-full bg-neutral-600 text-neutral-900 flex flex-col items-center py-10"
-    const divGrid = "grid grid-cols-3"
-    const buttonDesign = "w-24 h-24 bg-gray-900 rounded-lg text-white text-3xl flex items-center justify-center"
+  const parentDiv =
+    "min-h-screen w-full bg-neutral-600 text-neutral-900 flex flex-col items-center py-10";
+  const divGrid = "grid grid-cols-3";
+  const buttonDesign =
+    "w-24 h-24 bg-gray-900 rounded-lg text-white text-3xl flex items-center justify-center";
 
-      return(
+  return (
     <div className={parentDiv}>
-    <h1 className='text-5xl text-white font-semibold'>Tic Tac Toe</h1>
-    
-    <div className={divGrid}>
-      <button className={buttonDesign} onClick={() => handleClick(0, 0)} >{gameState.board[0][0]}</button>
-      <button className={buttonDesign} onClick={() => handleClick(0, 1)}>{gameState.board[0][1]}</button>
-      <button className={buttonDesign} onClick={() => handleClick(0, 2)} >{gameState.board[0][2]}</button>
-    </div>
-    
-    <div className={divGrid}>
-      <button className={buttonDesign} onClick={() => handleClick(1, 0)}>{gameState.board[1][0]}</button>
-      <button className={buttonDesign} onClick={() => handleClick(1, 1)}>{gameState.board[1][1]}</button>
-      <button className={buttonDesign} onClick={() => handleClick(1, 2)}>{gameState.board[1][2]}</button>     
-    </div>
+      <h1 className="text-5xl text-white font-semibold">Tic Tac Toe</h1>
 
-    <div className={divGrid}>
-      <button className={buttonDesign} onClick={() => handleClick(2, 0)}>{gameState.board[2][0]}</button>
-      <button className={buttonDesign} onClick={() => handleClick(2, 1)}>{gameState.board[2][1]}</button>
-      <button className={buttonDesign} onClick={() => handleClick(2, 2)}>{gameState.board[2][2]}</button>     
-    </div> 
+      <div className={divGrid}>
+        <button className={buttonDesign} onClick={() => handleClick(0, 0)}>
+          {gameState.board[0][0]}
+        </button>
+        <button className={buttonDesign} onClick={() => handleClick(0, 1)}>
+          {gameState.board[0][1]}
+        </button>
+        <button className={buttonDesign} onClick={() => handleClick(0, 2)}>
+          {gameState.board[0][2]}
+        </button>
+      </div>
 
-    <div className='text-white text-3xl'>
-        {gameState.winner ? `Winner: ${gameState.winner}` : `Next player: ${gameState.currentPlayer}`}
+      <div className={divGrid}>
+        <button className={buttonDesign} onClick={() => handleClick(1, 0)}>
+          {gameState.board[1][0]}
+        </button>
+        <button className={buttonDesign} onClick={() => handleClick(1, 1)}>
+          {gameState.board[1][1]}
+        </button>
+        <button className={buttonDesign} onClick={() => handleClick(1, 2)}>
+          {gameState.board[1][2]}
+        </button>
+      </div>
+
+      <div className={divGrid}>
+        <button className={buttonDesign} onClick={() => handleClick(2, 0)}>
+          {gameState.board[2][0]}
+        </button>
+        <button className={buttonDesign} onClick={() => handleClick(2, 1)}>
+          {gameState.board[2][1]}
+        </button>
+        <button className={buttonDesign} onClick={() => handleClick(2, 2)}>
+          {gameState.board[2][2]}
+        </button>
+      </div>
+
+      <div className="text-white text-3xl">
+        {gameState.winner
+          ? `Winner: ${gameState.winner}`
+          : `Next player: ${gameState.currentPlayer}`}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Game
+export default Game;
--- react/src/server.ts
+++ react/src/server.ts
@@ -1,29 +1,44 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import { initialGameState, makeMove, type GameState } from "./tictactoe";
-import { callWinner } from "./tictactoe";
+import { createInitialGameState, makeMove, callWinner, type GameState } from "./tictactoe";
 
-const app = express()
-const PORT = 3001
-
-let game = structuredClone(initialGameState)
-
-app.use(express.json())
-
-app.get("/api/game", (_, res) => {
-    res.json(game)
-})
 
 type MoveReqBody = {
-    row: number,
-    col: number
-}
+  row: number;
+  col: number;
+};
 
-app.post("/api/move", (req, res) => {
-    const body = req.body as MoveReqBody
-    game = makeMove(game, body.row, body.col)
-    res.status(201).json(game)
-})
+const app = express();
+const PORT = 3001;
+
+const games = new Map<number, GameState>()
+let nextID = 1
+
+const seedId = nextID++
+const newGame = createInitialGameState(seedId)
+games.set(seedId, newGame)
+
+app.use(express.json());
+
+app.get("/api/game/:id", (req, res) => {
+    const paramID = Number(req.params.id)
+    const game = games.get(paramID)
+    if (!game) return res.status(404).json({ error: "Game not found" })
+    res.json(game);
+});
+
+app.post("/api/move/:id", (req, res) => {
+  const paramId = Number(req.params.id)
+  const g = games.get(paramId)
+  const body = req.body as MoveReqBody;
+// to solve the type error with g
+  if (!g) return res.status(404).json({ error: "Game not found" })
+
+  const next = makeMove(g, body.row, body.col);
+  next.winner = callWinner(next.board)
+  games.set(paramId, next)
+  res.status(201).json(next);
+});
 
 ViteExpress.listen(app, PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
