35075567bd376dccb8d63be42ed15321f160ad95
Mohamad Abdou <mohamedabdouelbermawy@gmail.com>
2025-09-24 20:52:32 -0400

    LETS GOOO

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react"
 import Game from "./Game";
+import GameLobby from "/Users/mohamed/Documents/fractal-fa25/tic-tac-toe1/react/src/GameLobby.tsx"
 
 export default function App() { 
-  return <Game />
+  const [selectedGame, setSelectedGame] = useState<number|null>(null)   //that way if it's null it renders the list
+
+  async function handleCreateGame() {
+  const newGame = await fetch("/api/game", { method: "POST" })
+    .then((r) => r.json());
+  setSelectedGame(newGame.id);
+}
+  
+return selectedGame=== null ? (
+  <GameLobby 
+    onPick={setSelectedGame} 
+    onCreateClick={handleCreateGame} 
+  />
+) : (
+  <Game 
+    gameID={selectedGame} 
+    onBack={() => setSelectedGame(null)} 
+  />
+)
+
 }
---
+++ react/src/GameLobby.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import type { GameState } from "./tictactoe";
+import { initialGameState } from "./tictactoe";
+  type Player = "X" | "O"
+  type GameSummary = { id: number; winner: Player | undefined }
+  type LobbyProps = {
+    onPick: (id: number) => void,
+    onCreateClick: () => void
+  }
+
+export default function GameLobby({onPick, onCreateClick}: LobbyProps) {
+  const [gameList, setGameList] = useState<GameSummary[]>([]);
+
+  useEffect(() => {
+    fetch(`/api/game`)
+      .then((res) => res.json())
+      .then((data: GameSummary[]) => setGameList(data))
+      .catch((err) => console.error("error", err));
+  }, []);
+
+      const parentDiv =
+    "min-h-screen w-full bg-neutral-600 text-neutral-900 flex flex-col items-center py-10"
+
+      return (
+    <div className={parentDiv}>
+      <h1 className="text-3xl text-white font-bold mb-2">Tic-Tac-Toe Lobby</h1>
+      <p className="mb-6 text-white">
+        Join an existing game or start a new one.
+      </p>
+
+      <button
+        className="bg-[#2e2e2e] hover:bg-black px-4 py-2 rounded mb-6 text-white"
+        onClick={onCreateClick}
+      >
+        Create New Game
+      </button>
+
+      <ul className="w-full max-w-md divide-y divide-gray-700">
+        {gameList.map((g) => (
+          <li
+            key={g.id}
+            className="flex justify-between items-center py-3 px-4 hover:bg-gray-800 cursor-pointer"
+          >
+            <span className="font-medium text-white">Game #{g.id}</span>
+            <span className="text-sm text-gray-400">
+              {g.winner ? `${g.winner} won` : "In progress"}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
